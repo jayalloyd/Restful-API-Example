b407af0d3eb3eb631f2dc29a4fc962054588ba16
jayarani.ys <jayaraniys1@gmail.com>
2025-04-09 11:11:51 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -14,11 +16,12 @@ app.get("/posts/new",(req,res)=>{
 });
 app.post("/posts",(req,res)=>{
     let{username,content}=req.body;
-    posts.push({username,content});
+    let id= uuidv4(); 
+    posts.push({id,username,content});
     res.redirect("/posts")
 });
-let posts = [{id:"1", username: "rooney",content:"I love coding"},
-    {id:"2" ,username: "tuffy",content:"I love reading"},];
+let posts = [{id:uuidv4() , username: "rooney",content:"I love coding"},
+    {id:uuidv4() ,username: "tuffy",content:"I love reading"},];
 
 app.get("/posts/:id",(req,res)=>{
     let{id}=req.params;
